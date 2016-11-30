c84e56dcf0e6b796cb46489d447ac2aa07f1639a
Dmytro_Rud <fUrw3J231>
2016-11-30 16:23:09 +0200

    Templating Links & Images

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,17 +1,29 @@
 'use strict';
 
+
 angular.module('phoneList').component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-        this.phones = [
-            {
-                name: 'Nexus',
-                snippet: 'An android smartphone'
-            },
-            {
-                name: 'iPhone 7',
-                snippet: 'Apple smartphone'
-            }
-        ];
+    controller: function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+        $http.get('phones/phones.json').then(function (response) {
+            self.phones = response.data;
+        });
+        // this.phones = [
+        //     {
+        //         name: 'Nexus',
+        //         snippet: 'An android smartphone',
+        //         age: 1
+        //     }, {
+        //         name: 'iPhone 7',
+        //         snippet: 'Apple smartphone',
+        //         age: 2
+        //     }, {
+        //         name: 'Xiaomi Redmi Note 3 Pro',
+        //         snippet: 'Smartpone from China',
+        //         age: 3
+        //     }
+        // ];
+        // this.orderProp = 'age';
     }
 });
